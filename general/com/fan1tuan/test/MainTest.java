255c13b56fd133f9d39dbb6488711b7dbbd53f29
He Li <476282190@qq.com>
2014-01-26 23:56:11 +0800

    add greeting in MainTest.java

--- general/com/fan1tuan/test/MainTest.java
+++ general/com/fan1tuan/test/MainTest.java
@@ -7,6 +7,6 @@ public class MainTest {
 	public static void main(String[] args){
 		String url = "http://localhost:8080/CarsiLogCenter_new/idpstat.jsp?action=idp.sptopn&greeting=中文哦";
 		System.err.println(StringUtil.decodeURL(StringUtil.encodeURL(url)));
-		
+		System.out.println("Hello to you");
 	}
 }
