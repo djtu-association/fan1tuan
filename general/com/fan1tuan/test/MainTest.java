8171ffc080f65669146eab1aeea801792922842a
He Li <476282190@qq.com>
2014-01-27 13:55:21 +0800

    There is a bomb in MainTest.java

--- general/com/fan1tuan/test/MainTest.java
+++ general/com/fan1tuan/test/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
 	public static void main(String[] args){
 		String url = "http://localhost:8080/CarsiLogCenter_new/idpstat.jsp?action=idp.sptopn&greeting=中文哦";
 		System.err.println(StringUtil.decodeURL(StringUtil.encodeURL(url)));
-		System.err.print("hate you!!");
-		System.out.print("hate you!!");
+		
+		System.out.println("Houhou, It's a good day!!!!!");
 	}
 }
