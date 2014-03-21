93e10584259ccc40fb0e10de1c13f839ea3b2152
He Li <476282190@qq.com>
2014-03-21 14:52:28 +0800

    #issues5 -- fields now added. both for shop and dish entities.

--- shop/com/fan1tuan/shop/pojos/Dish.java
+++ shop/com/fan1tuan/shop/pojos/Dish.java
@@ -1,5 +1,6 @@
 package com.fan1tuan.shop.pojos;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 import com.fan1tuan.general.pojos.EntityObject;
@@ -24,9 +25,18 @@ public class Dish extends EntityObject{
 	private int status; //0不可预定,1可预订
 	private String description;
 	
+	private Date createTime;
 	
 	
 	
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
 	public String getDescription() {
 		return description;
 	}
